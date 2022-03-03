59e197a0ea0e78a5875e3501651be21347820096
Roely Chur <gerson.roely@gmail.com>
2022-03-03 11:27:16 -0600

    Módulo de Revisión de Forma

---
+++ src/views/Revision_Forma.vue
@@ -0,0 +1,128 @@
+
+<template>
+	<div>
+		<DataCard>
+
+			<template #table>
+
+				<!-- Filtro -->
+
+				<Filtro @update="fetchData()" />
+
+				<Table :data="data_table_filter">
+
+					<template #action="item">
+						<v-btn
+							text
+							small
+							color="primary"
+							:to="{name: 'document_detail_check', params: {id: item.documentoid}}"
+						>
+							<v-icon>mdi-information</v-icon>
+						</v-btn>
+					</template>
+
+					<template #estado="item">
+						<v-chip :color="item.estado.color" label>
+							{{ item.estado.nombre }}
+						</v-chip>
+					</template>
+
+					<template #tipo_documento="item">
+						<v-chip label>
+							{{ item.tipo_documento }}
+						</v-chip>
+					</template>
+
+				</Table>
+			</template>
+
+		</DataCard>
+			
+		<Modal fullscreen :scrollable="true" max_width="100px" width="900">
+			<template #content>
+				<Form :fetchData="fetchDataAction" :fetchParams="null"></Form>
+			</template>
+		</Modal>
+	</div>
+</template>
+
+<script>
+
+	import DataCard from '@/components/Home/DataCard'
+	import Table from '@/components/Table/Table'
+	import Modal from '@/components/Modal'
+	import Form from '@/components/UploadDocument/Form'
+	import Filtro from '@/components/Check/Filter'
+
+	import { mapState, mapActions, mapMutations } from 'vuex'
+
+	export default {
+		components: {
+			DataCard,
+			Table,
+			Modal,
+			Form,
+			Filtro
+		},
+		data(){
+			return{
+				fetchDataAction: 'fetchData'
+			}
+		},
+		methods: {
+			...mapActions({
+				fetchData: 'check/fetchData',
+				fetchFormDocument: 'upload_document/fetchFormDocument',
+				fetchDetail: 'document_detail/fetchDetail'
+			}),
+			...mapMutations({
+				setShow: 'modal/setShow',
+				setArea: 'filter/setArea'
+			}),
+			showForm(){
+
+				this.fetchFormDocument()
+				
+				this.setShow(true)
+			}
+		},
+		computed: {
+			...mapState({
+				data_table: state => state.check.data_table,
+				area: state => state.filter.area,
+				tipo: state => state.filter.tipo,
+				estado: state => state.filter.estado
+			}),
+			data_table_filter: function(){
+
+				if (this.data_table.items && (this.tipo || this.estado)) {
+					
+					let result = this.data_table.items.filter(item => this.tipo && this.estado ? (item.tipodocumentoid == this.tipo && item.estadoid == this.estado) : (item.tipodocumentoid == this.tipo || item.estadoid == this.estado))
+					
+					return {
+						headers: this.data_table.headers,
+						items: result
+					}
+
+				}
+				
+				return this.data_table
+				
+			}
+		},
+		mounted(){
+
+			if (!this.area) {
+				
+				const userData = JSON.parse(localStorage.getItem('app-documentos-iso'))
+
+				this.setArea(userData.codarea)
+
+			}		
+
+			this.fetchData()
+
+		}
+	}
+</script>
